69993847e294cdfb2708a9937d2f7f291babb9e1
78faysal <faysal7830@gmail.com>
2024-05-22 00:40:52 +0600

    cors origin link added

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // midlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://daily-extendure.web.app",
+      "https://daily-extendure.firebaseapp.com",
+    ],
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jq69c8i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -36,39 +43,31 @@ async function run() {
       .db("DailyExtendure")
       .collection("deletedData");
 
-
-
-
-      // middlewares 
-      const varifyToken = (req, res, next) => {
-        
-        if(!req.headers?.authorization){
-          return res.status(401).send({message: "Unauthorized Access"})
-        }
-
-        const token = req.headers?.authorization.split(" ")[1];
-
-        jwt.verify(token, process.env.JWT_SECRET_TOKEN, (err, decoded) => {
-          if(err){
-            return res.status(403).send({message: 'Forbidden Access'})
-          }
-          req.decoded = decoded;
-          next();
-        })
+    // middlewares
+    const varifyToken = (req, res, next) => {
+      if (!req.headers?.authorization) {
+        return res.status(401).send({ message: "Unauthorized Access" });
       }
 
-      
+      const token = req.headers?.authorization.split(" ")[1];
 
-      // jwt api
-      app.post('/jwt', async(req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.JWT_SECRET_TOKEN, {
-          expiresIn: '10h'
-        });
-        res.send({token})
-      })
+      jwt.verify(token, process.env.JWT_SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(403).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
 
-
+    // jwt api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET_TOKEN, {
+        expiresIn: "10h",
+      });
+      res.send({ token });
+    });
 
     // users api
     app.get("/userInfo", varifyToken, async (req, res) => {
@@ -101,7 +100,7 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/products",varifyToken, async (req, res) => {
+    app.post("/products", varifyToken, async (req, res) => {
       const productInfo = req.body;
       const { sku } = productInfo;
       // console.log(productInfo);
@@ -115,7 +114,7 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/products/:id",varifyToken, async (req, res) => {
+    app.patch("/products/:id", varifyToken, async (req, res) => {
       const updatedInfo = req.body;
       // console.log(updatedInfo);
       const id = req.params.id;
@@ -156,7 +155,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/products/:id",varifyToken, async (req, res) => {
+    app.delete("/products/:id", varifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const product = await productCollection.findOne(filter);
@@ -189,22 +188,28 @@ async function run() {
       res.send(result);
     });
 
-
     // customar info
-    app.get('/statistics', varifyToken, async(req, res) => {
+    app.get("/statistics", varifyToken, async (req, res) => {
       const email = req.query?.email;
 
-      const query = {seller: email};
+      const query = { seller: email };
       const customers = await purchaseCollection.find(query).toArray();
-      const customersInfo = customers?.map(item => ({buyer: item?.buyer, buyerName: item?.buyerName, buyerImage: item?.buyerImage}));
+      const customersInfo = customers?.map((item) => ({
+        buyer: item?.buyer,
+        buyerName: item?.buyerName,
+        buyerImage: item?.buyerImage,
+      }));
       const totalSell = await purchaseCollection.find(query).count();
       const availableProducts = await productCollection.find(query).count();
-      const prices = customers?.map(item => parseFloat(item?.price));
-      const totalAmount = prices.length > 0 ? prices?.reduce((total, current) => total + current) : 0;
-      
+      const prices = customers?.map((item) => parseFloat(item?.price));
+      const totalAmount =
+        prices.length > 0
+          ? prices?.reduce((total, current) => total + current)
+          : 0;
+
       // console.log(customers);
-      res.send({customersInfo, totalSell, totalAmount, availableProducts})
-    })
+      res.send({ customersInfo, totalSell, totalAmount, availableProducts });
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
